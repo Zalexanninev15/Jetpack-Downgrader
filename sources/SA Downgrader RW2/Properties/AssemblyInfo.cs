49312e0e8591616b90e4aae6e17186684f671cff
Zalexanninev15 <blue.shark@disroot.org>
2021-01-22 20:08:38 +0300

    Version 0.1.4.1
    - Added an icon for the app
    - Changed the title text of the console

--- sources/SA Downgrader RW2/Properties/AssemblyInfo.cs
+++ sources/SA Downgrader RW2/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.4.1")]
+[assembly: AssemblyFileVersion("0.1.4.1")]
