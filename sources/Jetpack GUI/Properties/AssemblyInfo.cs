6b177399bdae07daf9f082fdf20e658d316ab7f3
Zalexanninev15 <blue.shark@disroot.org>
2021-02-10 10:40:45 +0300

    Version 1.5-Dev 1
    - Downgrader [CF] updated to version 2.0-Alpha (no patches, but work is underway)
    - Minor edits
    
    * CF - Command Function

--- sources/Jetpack GUI/Properties/AssemblyInfo.cs
+++ sources/Jetpack GUI/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанных со сборкой.
 [assembly: AssemblyTitle("Jetpack GUI")]
 [assembly: AssemblyDescription("Graphical interface for Grand Theft Auto - San Andreas downgrader")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Jetpack GUI (2021)")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, следует установить атрибут ComVisible в TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("cb2fa897-3171-48cd-b4f7-5a34b9c9646a")]
 
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
