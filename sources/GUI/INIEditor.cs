a626976a0ff74bc09145a191b23c7337a8ef24a7
Zalexanninev15 <blue.shark@disroot.org>
2021-02-22 21:18:04 +0300

    Preparing for the future

---
+++ sources/GUI/INIEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace JetpackDowngraderGUI
+{
+    public class INIEditor
+    {
+        private const int SIZE = 1024;
+        private string path = null;
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetValue(string section, string key, string def, StringBuilder buffer, int size, string path);
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+        private static extern int WritePrivateString(string section, string key, string str, string path);
+
+        public INIEditor(string aPath) { path = aPath; }
+
+        public string GetValue(string aSection, string aKey)
+        {
+            StringBuilder buffer = new StringBuilder(SIZE);
+            GetValue(aSection, aKey, null, buffer, SIZE, path);
+            return buffer.ToString();
+        }
+
+        public void WritePrivateString(string aSection, string aKey, string aValue)
+        {
+            WritePrivateString(aSection, aKey, aValue, path);
+        }
+    }
+}
---
+++ sources/GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿#region Using directives
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("Jetpack Downgrader GUI")]
+[assembly: AssemblyDescription("GUI for Jetpack Downgrader")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zalexanninev15")]
+[assembly: AssemblyProduct("Jetpack Downgrader GUI")]
+[assembly: AssemblyCopyright("Copyright 2021, Zalexanninev15")]
+[assembly: AssemblyTrademark("Jetpack Downgrader GUI (2021)")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
