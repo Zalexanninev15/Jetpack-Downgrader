cd2daa5c1d9f822689f6b8dc4f56a2ba3f71347d
Zalexanninev15 <blue.shark@disroot.org>
2021-04-26 14:25:30 +0300

    JPD GUI Version 0.2.2-PreAlpha
    Changes:
    - Added basic features for using localizations
    - Added quite a few basic features
    - Added new interface elements
    - Improved support for JPD version 1.11 [DEV 1.4]
    - Minor improvements

--- sources/GUI/MainForm.cs
+++ sources/GUI/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using System.Windows.Forms;
 
@@ -6,6 +7,7 @@ namespace JetpackDowngraderGUI
 {
     public partial class MainForm : Form
     {
+        string[] lc = new string[100];
         public MainForm() { InitializeComponent(); }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -13,34 +15,33 @@ namespace JetpackDowngraderGUI
             try
             {
                 // Loading the localization
-                string[] lc = new string[100];
-                INIEditor lang = new INIEditor(@Application.StartupPath + @"\languages\" + Properties.Settings.Default.LanguageCode + ".jpl");
-                // Information loading
-
-                // Text loading
+                INIEditor lang = new INIEditor(@Application.StartupPath + @"\languages\" + Properties.Settings.Default.LanguageCode + ".txt");
+                // Text (GUI) loading
 
                 // Title loading
                 lc[0] = lang.GetValue("Title", "Info");
                 lc[1] = lang.GetValue("Title", "Error");
                 // InfoMsg loading
-
+                lc[4] = lang.GetValue("InfoMsg", "Succes");
                 // ErrorMsg loading
-
+                lc[2] = lang.GetValue("ErrorMsg", "ReadINI");
+                lc[3] = lang.GetValue("ErrorMsg", "WriteINI");
+                lc[5] = lang.GetValue("ErrorMsg", "BrowserNotFound");
             }
             catch { MessageBox.Show("Error loading the localization file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); Application.Exit(); }
             // Loading settings
             bool[] settings = new bool[16];
             try
             {
-                INIEditor cfg = new INIEditor(@Application.StartupPath + @"\jpd.ini");
+                INIEditor cfg = new INIEditor(@Application.StartupPath + @"\app\jpd.ini");
                 settings[2] = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateBackups"));
-                //settings[0] = Convert.ToBoolean(cfg.GetValue("Downgrader", "SetReadOnly"));
                 settings[6] = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateShortcut"));
                 settings[7] = Convert.ToBoolean(cfg.GetValue("Downgrader", "ResetGame"));
                 settings[14] = Convert.ToBoolean(cfg.GetValue("Downgrader", "RGLGarbage"));
                 settings[9] = Convert.ToBoolean(cfg.GetValue("Downgrader", "RegisterGamePath"));
                 settings[10] = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateNewGamePath"));
                 settings[12] = Convert.ToBoolean(cfg.GetValue("Downgrader", "Forced"));
+                settings[0] = Convert.ToBoolean(cfg.GetValue("Downgrader", "EnableDirectPlay"));
                 settings[8] = Convert.ToBoolean(cfg.GetValue("JPD", "SelectFolder"));
                 settings[11] = Convert.ToBoolean(cfg.GetValue("JPD", "ConsoleTransparency"));
                 settings[13] = Convert.ToBoolean(cfg.GetValue("JPD", "UseMsg"));
@@ -50,7 +51,25 @@ namespace JetpackDowngraderGUI
                 settings[4] = Convert.ToBoolean(cfg.GetValue("Only", "NextCheckFiles"));
                 settings[5] = Convert.ToBoolean(cfg.GetValue("Only", "NextCheckFilesAndCheckMD5"));
             }
-            catch { /* Error Msg */ }
+            catch { MsgError(lc[2], lc[1]); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(@Application.StartupPath + @"\app\jpd.exe").WaitForExit();
+            // Install mods
+
+            //
+            MsgInfo(lc[4], lc[0]);
+        }
+
+        private void button5_Click(object sender, EventArgs e) { try { Process.Start("https://github.com/Zalexanninev15/Jetpack-Downgrader"); } catch { MsgError(lc[5], lc[1]); } }
+        private void MsgInfo(string message, string title) { MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information); }
+        private void MsgError(string message, string title) { MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Change language
         }
     }
 }
