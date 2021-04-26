010360e52885ba6b412d500c67ce48a5df2619d6
Zalexanninev15 <maks.shabarchin@yandex.ru>
2021-04-26 20:17:43 +0300

    Version 1.11 [DEV 1.5]
    Changes:
    - Added 2 new settings
    - Added a reference to the license
    - Added link to the repository
    - Updated the game folder selection dialog (tested on Windows 10)
    - Improved removal of game settings
    - Improved code optimization and overall performance
    - Minor improvements for other developers (if Component=true and SelectFolder=false)
    - Other improvements

--- sources/Downgrader/Properties/AssemblyInfo.cs
+++ sources/Downgrader/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Jetpack Downgrader (2020-2021)")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.11.0.4")]
-[assembly: AssemblyFileVersion("1.11.0.4")]
+[assembly: AssemblyVersion("1.11.0.5")]
+[assembly: AssemblyFileVersion("1.11.0.5")]
