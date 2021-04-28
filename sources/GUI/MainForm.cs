d982858620836a539243219323a433016a2a12de
Zalexanninev15 <blue.shark@disroot.org>
2021-04-28 21:30:33 +0300

    JPD GUI Version 0.2.4-PreAlpha
    Changes:
    - GUI was a merger with the main project. Now the development of new builds will take place approximately in parallel, which will significantly speed up the overall development of the application
    - Added a basic setting of settings for checkboxes
    - Improved working with the settings of the latest version of the Downgrader
    - Improved description of checkboxes
    - Fixed the perception of encodings when loading localizations. The work was checked in Russian text
    [!] Temporarily the appearance was changed and the column with modifications moved to another location

--- sources/GUI/MainForm.cs
+++ sources/GUI/MainForm.cs
@@ -8,6 +8,7 @@ namespace JetpackDowngraderGUI
     public partial class MainForm : Form
     {
         string[] lc = new string[100];
+        bool[] appset = new bool[8];
         public MainForm() { InitializeComponent(); }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -15,7 +16,7 @@ namespace JetpackDowngraderGUI
             try
             {
                 // Loading the localization
-                INIEditor lang = new INIEditor(@Application.StartupPath + @"\languages\" + Properties.Settings.Default.LanguageCode + ".txt");
+                IniEditor lang = new IniEditor(@Application.StartupPath + @"\languages\" + Properties.Settings.Default.LanguageCode + ".txt");
                 // Text (GUI) loading
                 label1.Text = Convert.ToString(lang.GetValue("Interface", "PathLabel"));
                 button2.Text = Convert.ToString(lang.GetValue("Interface", "CButton"));
@@ -32,26 +33,27 @@ namespace JetpackDowngraderGUI
             }
             catch { MessageBox.Show("Error loading the localization file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); Application.Exit(); }
             // Loading settings
-            bool[] settings = new bool[16];
             try
             {
-                INIEditor cfg = new INIEditor(@Application.StartupPath + @"\app\jpd.ini");
-                settings[2] = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateBackups"));
-                settings[6] = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateShortcut"));
-                settings[7] = Convert.ToBoolean(cfg.GetValue("Downgrader", "ResetGame"));
-                settings[14] = Convert.ToBoolean(cfg.GetValue("Downgrader", "RGLGarbage"));
-                settings[9] = Convert.ToBoolean(cfg.GetValue("Downgrader", "RegisterGamePath"));
-                settings[10] = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateNewGamePath"));
-                settings[12] = Convert.ToBoolean(cfg.GetValue("Downgrader", "Forced"));
-                settings[0] = Convert.ToBoolean(cfg.GetValue("Downgrader", "EnableDirectPlay"));
-                settings[8] = Convert.ToBoolean(cfg.GetValue("JPD", "SelectFolder"));
-                settings[11] = Convert.ToBoolean(cfg.GetValue("JPD", "ConsoleTransparency"));
-                settings[13] = Convert.ToBoolean(cfg.GetValue("JPD", "UseMsg"));
-                settings[15] = Convert.ToBoolean(cfg.GetValue("JPD", "UseProgressBar"));
-                settings[1] = Convert.ToBoolean(cfg.GetValue("JPD", "Component"));
-                settings[3] = Convert.ToBoolean(cfg.GetValue("Only", "GameVersion"));
-                settings[4] = Convert.ToBoolean(cfg.GetValue("Only", "NextCheckFiles"));
-                settings[5] = Convert.ToBoolean(cfg.GetValue("Only", "NextCheckFilesAndCheckMD5"));
+
+                IniEditor cfg = new IniEditor(@Application.StartupPath + @"\app\jpd.ini");
+                checkBox1.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateBackups"));
+                checkBox2.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateShortcut"));
+                checkBox9.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "ResetGame"));
+                checkBox4.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "RGLGarbage"));
+                checkBox6.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "RegisterGamePath"));
+                checkBox3.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "CreateNewGamePath"));
+                checkBox5.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "Forced"));
+                checkBox7.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "EnableDirectPlay"));
+                checkBox8.Checked = Convert.ToBoolean(cfg.GetValue("Downgrader", "InstallDirectX"));
+                appset[0] = Convert.ToBoolean(cfg.GetValue("JPD", "SelectFolder"));
+                appset[1] = Convert.ToBoolean(cfg.GetValue("JPD", "ConsoleTransparency"));
+                appset[2] = Convert.ToBoolean(cfg.GetValue("JPD", "UseMsg"));
+                appset[3] = Convert.ToBoolean(cfg.GetValue("JPD", "UseProgressBar"));
+                appset[4] = Convert.ToBoolean(cfg.GetValue("JPD", "Component"));
+                appset[5] = Convert.ToBoolean(cfg.GetValue("Only", "GameVersion"));
+                appset[6] = Convert.ToBoolean(cfg.GetValue("Only", "NextCheckFiles"));
+                appset[7] = Convert.ToBoolean(cfg.GetValue("Only", "NextCheckFilesAndCheckMD5"));
             }
             catch { MsgError(lc[2], lc[1]); }
         }
