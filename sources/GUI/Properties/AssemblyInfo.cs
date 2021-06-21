dadf0db5588aef13901f10e8ed0cfee011d37c61
Max Shabarchin <maks.shabarchin@yandex.ru>
2021-06-21 15:34:46 +0300

    GUI Version 0.7.5 (Beta)
    Changes:
    - Now the application can be localized to 100%, well, or at least 98% :)
    - Improvements
    - Fixed all bugs in version 0.7 (Beta)
    - Unnecessary resources removed

--- sources/GUI/Properties/AssemblyInfo.cs
+++ sources/GUI/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Jetpack Downgrader (2020-2021)")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.7.5.0")]
+[assembly: AssemblyFileVersion("0.7.5.0")]
