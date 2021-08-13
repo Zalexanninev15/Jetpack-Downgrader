4ab85fb035e7ec0151f4041c05a6330661cac9de
Max Shabarchin <maks.shabarchin@yandex.ru>
2021-08-13 14:58:20 +0300

    Preparing for the release | Version 2.2

--- sources/Downgrader/Classes/SettingsReader.cs
+++ sources/Downgrader/Classes/SettingsReader.cs
@@ -7,40 +7,28 @@ namespace JetpackDowngrader
     {
         [XmlElement(ElementName = "CreateBackups")]
         public bool CreateBackups { get; set; }
-
         [XmlElement(ElementName = "CreateShortcut")]
         public bool CreateShortcut { get; set; }
-
         [XmlElement(ElementName = "ResetGame")]
         public bool ResetGame { get; set; }
-
         [XmlElement(ElementName = "RGL_GarbageCleaning")]
         public bool RGLGarbageCleaning { get; set; }
-
         [XmlElement(ElementName = "RegisterGamePath")]
         public bool RegisterGamePath { get; set; }
-
         [XmlElement(ElementName = "CopyGameToNewPath")]
         public bool CopyGameToNewPath { get; set; }
-
         [XmlElement(ElementName = "EnableDirectPlay")]
         public bool EnableDirectPlay { get; set; }
-
         [XmlElement(ElementName = "InstallDirectXComponents")]
         public bool InstallDirectXComponents { get; set; }
-
         [XmlElement(ElementName = "Forced")]
         public bool Forced { get; set; }
-
         [XmlElement(ElementName = "UserMode")]
         public bool UserMode { get; set; }
-
         [XmlAttribute(AttributeName = "xsd")]
         public string Xsd { get; set; }
-
         [XmlAttribute(AttributeName = "xsi")]
         public string Xsi { get; set; }
-
         [XmlText]
         public string Text { get; set; }
     }
