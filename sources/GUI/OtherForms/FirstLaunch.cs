1601da416aa94b41c43784040b7a06645ec44e58
Max <maks.shabarchin@yandex.ru>
2021-09-22 10:26:47 +0300

    GUI Version 1.2.3.11 [DEV 2.1]
    Changes:
    - Added checking for updates (when launching the application)
    - Minor edit

--- sources/GUI/OtherForms/FirstLaunch.cs
+++ sources/GUI/OtherForms/FirstLaunch.cs
@@ -26,7 +26,7 @@ namespace JetpackGUI
                 System.Threading.Tasks.Task.Delay(300);
                 Application.Restart();
             }
-            else { DarkUI.Forms.DarkMessageBox.ShowWarning("You need to select a language from the list!", "Information"); }
+            else { DarkUI.Forms.DarkMessageBox.ShowInformation("You need to select a language from the list!", "Information"); }
         }
 
         void MyLang_Load(object sender, EventArgs e)
---
+++ sources/GUI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace JetpackGUI
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool AvailableNewVersion = false;
+            const bool DevVersion = false;
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                if (DevVersion == false)
+                {
+                    try
+                    {
+                        Ping ping = new Ping();
+                        PingReply pingReply = null;
+                        pingReply = ping.Send("github.com");
+                        using (WebClient wc = new WebClient())
+                        {
+                            string toolkit_version = wc.DownloadString("https://raw.githubusercontent.com/Zalexanninev15/Jetpack-Downgrader/unstable/Version.txt");
+                            if (toolkit_version != Data.JetpackDowngraderVersion)
+                            {
+                                AvailableNewVersion = true;
+                            }
+                        }
+                    }
+                    catch { AvailableNewVersion = false; }
+                }
+                if (AvailableNewVersion == false)
+                {
+                    GUI mygui = new GUI();
+                    mygui.ReadXml();
+                    if (mygui.Fields.FirstLaunch == false) { Application.Run(new MainForm()); } else { Application.Run(new MyLang()); }
+                }
+                else
+                {
+                    DarkUI.Forms.DarkMessageBox.ShowInformation("An update is available!\nNow you will be redirected to the download page of the latest version", "Information");
+                    try { System.Diagnostics.Process.Start("https://github.com/Zalexanninev15/Jetpack-Downgrader/releases/latest"); } catch { DarkUI.Forms.DarkMessageBox.ShowWarning("Browser to open the link was not found! The link will be copied to the clipboard!", "Warning"); Clipboard.SetText("https://github.com/Zalexanninev15/Jetpack-Downgrader/releases/latest"); }
+                }
+            }
+            catch(Exception ex) { DarkUI.Forms.DarkMessageBox.ShowError(ex.ToString(), "Error"); }
+        }
+    }
+}
