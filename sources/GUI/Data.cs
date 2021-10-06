3b7b09e7e5c3cb2edbbda9776fe92dc0f8214f46
Max <maks.shabarchin@yandex.ru>
2021-10-06 20:03:02 +0300

    Update Data.cs

--- sources/GUI/Data.cs
+++ sources/GUI/Data.cs
@@ -1,6 +1,6 @@
 ﻿static class Data
 {
-        public static string JetpackDowngraderVersion { get; } = "2.3";
+        public static string JetpackDowngraderVersion { get; } = "2.3.0.1";
         public static string PathToGame { get; set; }
         public static bool DebugMode { get; set; }
     }
